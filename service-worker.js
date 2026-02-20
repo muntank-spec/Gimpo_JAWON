85c77b95e56827472c569107a2b32b8a114411fb
muntank-spec <muntank@gmail.com>
2026-02-20 16:05:47 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gimpo-center-v1';
+const CACHE_NAME = 'gimpo-center-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -18,4 +18,5 @@ self.addEventListener('fetch', event => {
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
+
 });
