84ba7b03586842f48ba409e1307b083c7b7e5abe
MS.KO <muntank@gmail.com>
2026-02-23 13:27:47 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ self.addEventListener('activate', event => {
     })
   );
 });
-const CACHE_NAME = 'gimpo-center-v1';
+const CACHE_NAME = 'gimpo-center-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -43,6 +43,7 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
 
 
